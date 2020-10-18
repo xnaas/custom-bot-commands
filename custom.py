b8dce0c1464a84e2e7070045c1f6d8b08d0a7f23
xnaas <8271327+xnaas@users.noreply.github.com>
2020-10-18 00:40:15 -0500

    Sinners!

--- custom.py
+++ custom.py
@@ -188,3 +188,7 @@ def dragon(bot, trigger):
 def football(bot, trigger):
   football = ['D\'Marcus Williums', 'T.J. Juckson', 'T\'varisuness King', 'Jackmerius Tacktheritrix', 'D\'Squarius Green, Jr.', 'Dan Smith', 'The Player Formerly Known as Mousecop', 'Ibrahim Moizoos', 'D\'Isiah T. Billings-Clyde', 'D\'Jasper Probincrux III', 'Leoz Maxwell Jilliumz', 'Javaris Jamar Javarison-Lamar', 'Davoin Shower-Handel', 'Hingle McCringleberry', 'L\'Carpetron Dookmarriot', 'J\'Dinkalage Morgoone', 'Xmus Jaxon Flaxon-Waxon', 'Saggitariutt Jefferspin', 'D\'Glester Hardunkichud', 'Swirvithan L\'Goodling-Splatt', 'Quatro Quatro', 'Ozamataz Buckshank', 'Beezer Twelve Washingbeard', 'Shakiraquan T.G.I.F. Carter', 'X-Wing @Aliciousness', 'Sequester Grundelplith M.D.', 'Scoish Velociraptor Maloish', 'T.J. A.J. R.J. Backslashinfourth V', 'Eeeee Eeeeeeeee', 'Donkey Teeth', 'Torque [Construction Noise] Lewith', 'Tyroil Smoochie-Wallace']
   bot.say(random.choice(football))
+
+@rule('.*🍍🍕.*')
+def sin(bot, trigger):
+  bot.say('This is a sin.')
