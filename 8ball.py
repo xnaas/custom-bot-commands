159965357fb8815933a32c1d80ecd44f615c9a8b
xnaas <8271327+xnaas@users.noreply.github.com>
2020-11-09 16:02:42 -0600

    added '8ball' trigger

--- 8ball.py
+++ 8ball.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from sopel.module import commands, example, rule
 import random
 
-@commands('8')
+@commands('8', '8ball')
 @example('.8 am I gay?')
 def eightball(bot, trigger):
   """The magic 8ball knows all."""
