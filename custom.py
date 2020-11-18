6506ea70982e3b767eb235fa948d62a416bb7d4c
xnaas <8271327+xnaas@users.noreply.github.com>
2020-11-18 16:55:25 -0600

    cry me a river

--- custom.py
+++ custom.py
@@ -155,6 +155,10 @@ def crying(bot, trigger):
   ]
   bot.say(random.choice(crying))
 
+@module.rule(".*cry\sme\sa\sriver.*")
+def cryriver(bot, trigger):
+  bot.say("https://actionsack.com/img/QQ/QQ007.gif")
+
 @module.rule(".*!xms.*", ".*!bge.*")
 def xms(bot, trigger):
   bot.say("https://w2g.tv/rooms/actionsack-csurhbl9dkwvwnk91a")
