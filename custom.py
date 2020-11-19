bfb3365fc10d5ddb79958b9fd719db720edeb1ea
xnaas <8271327+xnaas@users.noreply.github.com>
2020-11-19 07:56:40 -0600

    not using itertools, no reason to import

--- custom.py
+++ custom.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 from sopel import module, formatting
 import random
-import itertools
 import unicodedata
 
 ### Section Exists for "judge" command only ###
