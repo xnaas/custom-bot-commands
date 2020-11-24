4dbd62ef4f2a28a0255a8d744ccd8fe17f49b39d
xnaas <8271327+xnaas@users.noreply.github.com>
2020-11-23 19:15:38 -0600

    add animals.py and update readme

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-## custom.py
-Custom commands for [Action Sack](https://actionsack.com)'s [Sopel bot](https://github.com/sopel-irc/sopel).
+# custom-sopel-plugins
+
+These are some [Sopel](https://github.com/sopel-irc/sopel) plugins I've written. While `custom.py` is very, very specific to my chat, the rest are pretty generic and a great addition to any IRC channel.
 
 ## 8ball.py
 Just a standard, run-of-the-mill [magic 8-ball](https://en.wikipedia.org/wiki/Magic_8-Ball). Never lies.
 
+## animals.py
+Posts random animal pics from several different APIs.
+
 ## colors.py
 Moved from [sopel-color-text](https://github.com/xnaas/sopel-color-text) — don't want to actually deal with publishing to pypi in the future.
+
+## custom.py
+Custom commands for [Action Sack](https://actionsack.com)'s Sopel bot.
---
+++ animals.py
@@ -0,0 +1,38 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+from sopel import module
+import requests
+
+@module.commands("cat")
+def cats(bot, trigger):
+  """Posts a random cat using the aws.random.cat/meow API."""
+  url = "https://aws.random.cat/meow"
+  cat_image = requests.get(url).json()['file']
+  bot.say(cat_image)
+
+@module.commands("catgif")
+def catgif(bot, trigger):
+  """Posts a random cat GIF using thecatapi.com API."""
+  url = "https://api.thecatapi.com/v1/images/search?mime_types=gif"
+  cat_gif = requests.get(url).json()[0]['url']
+  bot.say(cat_gif)
+
+@module.commands("dog")
+def dogs(bot, trigger):
+  """Posts a random dog using the dog.ceo/dog-api API."""
+  url = "https://dog.ceo/api/breeds/image/random"
+  dog_image = requests.get(url).json()['message']
+  bot.say(dog_image)
+
+@module.commands("shibe")
+def shibe(bot, trigger):
+  """Posts a random Shiba Inu using the shibe.online API."""
+  url = "https://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true"
+  shibe_image = requests.get(url).json()[0]
+  bot.say(shibe_image)
+
+@module.commands("fox", "foxy")
+def fox(bot, trigger):
+  """Posts a random fox using the randomfox.ca API."""
+  url = "https://randomfox.ca/floof/"
+  fox_image = requests.get(url).json()['image']
+  bot.say(fox_image)
