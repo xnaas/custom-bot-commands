c38a0592b4304664a6b69bf2a55933f2338385f9
xnaas <8271327+xnaas@users.noreply.github.com>
2020-12-13 13:21:12 -0600

    fixed nsfw.py to work in DMs and attains full-res images now

--- nsfw.py
+++ nsfw.py
@@ -6,19 +6,19 @@ import requests
 @module.commands("ass")
 def ass_api(bot, trigger):
   """Posts a random ass pic. #nsfw only."""
-  if trigger.sender == "#nsfw":
-    url = "http://api.obutts.ru/butts/%s" %random.randint(0, 8286)
-    ass_preview = requests.get(url).json()[0]['preview']
-    bot.say("http://media.obutts.ru/%s" %ass_preview)
+  if trigger.is_privmsg or trigger.sender == "#nsfw":
+    url = "http://api.obutts.ru/butts/0/1/random"
+    ass_id = str(requests.get(url).json()[0]["id"])
+    bot.say("http://media.obutts.ru/butts/%s.jpg" %ass_id.zfill(5))
   else:
     bot.say("This command is only usable in the #nsfw channel.")
 
 @module.commands("boobs")
 def boobs_api(bot, trigger):
   """Posts a random boobs pic. #nsfw only."""
-  if trigger.sender == "#nsfw":
-    url = "http://api.oboobs.ru/boobs/%s" %random.randint(0, 14620)
-    boobs_preview = requests.get(url).json()[0]['preview']
-    bot.say("http://media.oboobs.ru/%s" %boobs_preview)
+  if trigger.is_privmsg or trigger.sender == "#nsfw":
+    url = "http://api.oboobs.ru/boobs/0/1/random"
+    boobs_id = str(requests.get(url).json()[0]["id"])
+    bot.say("https://media.oboobs.ru/boobs/%s.jpg" %boobs_id.zfill(5))
   else:
     bot.say("This command is only usable in the #nsfw channel.")
