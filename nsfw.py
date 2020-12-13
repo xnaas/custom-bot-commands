dc226e6786a48a90a606a94b579b732e4e134b30
xnaas <8271327+xnaas@users.noreply.github.com>
2020-12-13 12:45:27 -0600

    fixed nsfw.py to only run in #nsfw

--- nsfw.py
+++ nsfw.py
@@ -4,15 +4,23 @@ import random
 import requests
 
 @module.commands("ass")
+@module.require_chanmsg
 def ass_api(bot, trigger):
   """Posts a random ass pic. NSFW, obviously."""
-  url = "http://api.obutts.ru/butts/%s" %random.randint(0, 8286)
-  ass_preview = requests.get(url).json()[0]['preview']
-  bot.say("http://media.obutts.ru/%s" %ass_preview)
+  if trigger.sender == "#nsfw":
+    url = "http://api.obutts.ru/butts/%s" %random.randint(0, 8286)
+    ass_preview = requests.get(url).json()[0]['preview']
+    bot.say("http://media.obutts.ru/%s" %ass_preview)
+  else:
+    bot.say("This command is only usable in the #nsfw channel.")
 
 @module.commands("boobs")
+@module.require_chanmsg
 def boobs_api(bot, trigger):
   """Posts a random boobs pic. NSFW, obviously."""
-  url = "http://api.oboobs.ru/boobs/%s" %random.randint(0, 14620)
-  boobs_preview = requests.get(url).json()[0]['preview']
-  bot.say("http://media.oboobs.ru/%s" %boobs_preview)
+  if trigger.sender == "#nsfw":
+    url = "http://api.oboobs.ru/boobs/%s" %random.randint(0, 14620)
+    boobs_preview = requests.get(url).json()[0]['preview']
+    bot.say("http://media.oboobs.ru/%s" %boobs_preview)
+  else:
+    bot.say("This command is only usable in the #nsfw channel.")
