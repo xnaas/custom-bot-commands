bf706d286f0bbb8ac22a6178bfea9ecb281796fd
xnaas <8271327+xnaas@users.noreply.github.com>
2020-12-13 18:01:02 -0600

    fixed how links are pulled in nsfw.py

--- nsfw.py
+++ nsfw.py
@@ -8,8 +8,9 @@ def ass_api(bot, trigger):
   """Posts a random ass pic. #nsfw only."""
   if trigger.is_privmsg or trigger.sender == "#nsfw":
     url = "http://api.obutts.ru/butts/0/1/random"
-    ass_id = str(requests.get(url).json()[0]["id"])
-    bot.say("http://media.obutts.ru/butts/%s.jpg" %ass_id.zfill(5))
+    ass_preview = requests.get(url).json()[0]["preview"]
+    ass_img = ass_preview.replace("_preview", "")
+    bot.say("http://media.obutts.ru/%s" %ass_img)
   else:
     bot.say("This command is only usable in the #nsfw channel.")
 
@@ -18,7 +19,8 @@ def boobs_api(bot, trigger):
   """Posts a random boobs pic. #nsfw only."""
   if trigger.is_privmsg or trigger.sender == "#nsfw":
     url = "http://api.oboobs.ru/boobs/0/1/random"
-    boobs_id = str(requests.get(url).json()[0]["id"])
-    bot.say("https://media.oboobs.ru/boobs/%s.jpg" %boobs_id.zfill(5))
+    boobs_preview = requests.get(url).json()[0]["preview"]
+    boobs_img = boobs_preview.replace("_preview", "")
+    bot.say("https://media.oboobs.ru/%s" %boobs_img)
   else:
     bot.say("This command is only usable in the #nsfw channel.")
