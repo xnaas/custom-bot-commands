2f92ef41ca592e43a856338edf75dae11cc9357f
xnaas <me@xnaas.info>
2021-01-09 14:43:33 -0600

    missed newhouse PNG → WebP

--- custom.py
+++ custom.py
@@ -1538,7 +1538,7 @@ def rimshot(bot, trigger):
 def newhouse(bot, trigger):
   """fecktk tells us about his new house.
   Can also be triggered with '!newhouse' anywhere in a message."""
-  bot.say("https://actionsack.com/img/fecktk/newhouse.png")
+  bot.say("https://actionsack.com/img/fecktk/newhouse.webp")
 
 @module.rule(".*!drone.*")
 @module.commands("drone")
