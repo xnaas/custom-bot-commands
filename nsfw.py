f4129cdc80d394e7214c844175c1c2a4433c6176
xnaas <me@xnaas.info>
2021-01-12 17:31:05 -0600

    add 'rboobs' for Reddit boobs

--- nsfw.py
+++ nsfw.py
@@ -26,3 +26,17 @@ def boobs_api(bot, trigger):
         bot.say("https://media.oboobs.ru/{}".format(boobs_img))
     else:
         bot.say("This command is only usable in the #nsfw channel.")
+
+@module.commands("rboobs")
+def reddit_boobs(bot, trigger):
+    """Posts a random boob pic from Reddit. #nsfw only."""
+    if trigger.is_privmsg or trigger.sender == "#nsfw":
+        url = "https://old.reddit.com/search.json?q=boobs&restrict_sr=&include_over_18=on&sort=hot&t=day&type=link&limit=100"
+        headers = {
+            "User-Agent": "python/requests",
+            "Content-Type": "application/json"
+        }
+        boobsr_img = requests.get(url, headers=headers).json()["data"]["children"][random.randrange(100)]["data"]["url"]
+        bot.say(boobsr_img)
+    else:
+        bot.say("This command is only usable in the #nsfw channel.")
