597b42cd7593834b1dd6d96a64e8be980ca592d3
xnaas <me@xnaas.info>
2021-02-09 17:42:05 -0600

    pep8'd

--- base64.py
+++ base64.py
@@ -2,15 +2,16 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from sopel import module
 import base64
 
+
 @module.commands("b64e")
 def base64_encode(bot, trigger):
     if not trigger.group(2):
         bot.reply("I need something to encode.")
         return
-    
+
     encodedBytes = base64.b64encode(trigger.group(2).encode("utf-8"))
     encodedStr = str(encodedBytes, "utf-8")
-    
+
     bot.say(encodedStr)
 
 
@@ -19,10 +20,10 @@ def base64_decode(bot, trigger):
     if not trigger.group(2):
         bot.reply("I need something to decode.")
         return
-    
+
     try:
         decodedBytes = base64.b64decode(trigger.group(2).encode("utf-8"))
         decodedStr = str(decodedBytes, "utf-8")
         bot.say(decodedStr)
-    except:
+    except BaseException:
         bot.reply("I need real base64, fool.")
