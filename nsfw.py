81aa7e5a829fdd1a3093239740c958af676924de
xnaas <me@xnaas.info>
2021-03-26 15:40:37 -0500

    fixed nsfw.py, but unsure why 'q' can't be passed with the long Reddit search string

--- nsfw.py
+++ nsfw.py
@@ -37,16 +37,16 @@ def boobs_api(bot, trigger):
 def reddit_boobs(bot, trigger):
     """Posts a random boob pic from Reddit. #nsfw only."""
     if trigger.is_privmsg or trigger.sender == "#nsfw":
-        url = "https://old.reddit.com/search.json"
+        url = "https://old.reddit.com/search.json?q=(boobs+OR+titties)+AND+nsfw%3Ayes+AND+(site%3Areddit.com+OR+site%3Aredgifs.com+OR+site%3Aimgur.com)"
         params = {
-            "q": "(boobs+OR+titties)+AND+nsfw%3Ayes+AND+(site%3Areddit.com+OR+site%3Aredgifs.com+OR+site%3Aimgur.com)",
+            "restrict_sr": "",
             "include_over_18": "on",
             "sort": "top",
             "t": "day",
             "type": "link",
-            "limit": "100"}
-        rboobs_img = requests.get(url, headers=headers, params=params).json()[
-            "data"]["children"][random.randrange(100)]["data"]["url"]
+            "limit": "100"
+        }
+        rboobs_img = requests.get(url, params=params, headers=headers).json()["data"]["children"][random.randrange(100)]["data"]["url"]
         bot.say(rboobs_img)
     else:
         bot.say("This command is only usable in the #nsfw channel.")
@@ -56,16 +56,16 @@ def reddit_boobs(bot, trigger):
 def reddit_ass(bot, trigger):
     """Posts a random ass pic from Reddit. #nsfw only."""
     if trigger.is_privmsg or trigger.sender == "#nsfw":
-        url = "https://old.reddit.com/search.json"
+        url = "https://old.reddit.com/search.json?q=(ass+OR+butt)+AND+nsfw%3Ayes+AND+(site%3Areddit.com+OR+site%3Aredgifs.com+OR+site%3Aimgur.com)"
         params = {
-            "q": "(ass+OR+butt)+AND+nsfw%3Ayes+AND+(site%3Areddit.com+OR+site%3Aredgifs.com+OR+site%3Aimgur.com)",
+            "restrict_sr": "",
             "include_over_18": "on",
             "sort": "top",
             "t": "day",
             "type": "link",
-            "limit": "100"}
-        rass_img = requests.get(url, headers=headers).json(
-        )["data"]["children"][random.randrange(100)]["data"]["url"]
+            "limit": "100"
+        }
+        rass_img = requests.get(url, params=params, headers=headers).json()["data"]["children"][random.randrange(100)]["data"]["url"]
         bot.say(rass_img)
     else:
         bot.say("This command is only usable in the #nsfw channel.")
