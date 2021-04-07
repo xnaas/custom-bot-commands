4ded61b14c00cb50f4afd7e67fb61dffb59fc2b3
xnaas <me@xnaas.info>
2021-04-07 15:54:09 -0500

    add error handling to ISBN, just in case ¯\_(ツ)_/¯

--- isbn.py
+++ isbn.py
@@ -7,11 +7,16 @@ import requests
 @module.commands("isbn")
 def isbn(bot, trigger):
     """Look up a book by its ISBN."""
-    if not re.match(r"((978[\--– ])?[0-9][0-9\--– ]{10}[\--– ][0-9xX])|((978)?[0-9]{9}[0-9Xx])", trigger.group(2)):
+    if not re.match(
+        r"((978[\--– ])?[0-9][0-9\--– ]{10}[\--– ][0-9xX])|((978)?[0-9]{9}[0-9Xx])",
+            trigger.group(2)):
         bot.reply("I need a valid ISBN.")
         return
 
     isbn_sanitized = trigger.group(2).replace(" ", "").replace("-", "")
     url = "https://openlibrary.org/isbn/{}".format(isbn_sanitized)
-    book = requests.get(url).url
-    bot.say(book)
+    try:
+        book = requests.get(url).url
+        bot.say(book)
+    except BaseException:
+        bot.reply("Error reaching API, probably.")
