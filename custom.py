6e794db090baf39d64ed9734aef39229a662e7da
xnaas <me@xnaas.info>
2021-04-26 12:24:54 -0500

    change '#' to '%23'

--- custom.py
+++ custom.py
@@ -2427,7 +2427,7 @@ def heff(bot, trigger):
 def salty(bot, trigger):
     """Can also be triggered with '!salty' anywhere in a message."""
     salty = [
-        "https://actionsack.com/img/salt/#staysalty.png",
+        "https://actionsack.com/img/salt/%23staysalty.png",
         "https://actionsack.com/img/salt/all-these-flavors.png",
         "https://actionsack.com/img/salt/dance.webp",
         "https://actionsack.com/img/salt/power-salt.gif",
