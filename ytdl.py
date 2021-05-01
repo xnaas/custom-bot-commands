c4e2ca218a2ef4acbd4b4614e4af21841ff72008
xnaas <me@xnaas.info>
2021-04-30 22:35:09 -0500

    first pass at abuse prevention: prevent downloading youtube/twitch livestreams, limit twitch/youtube videos to 10 minutes in length

--- ytdl.py
+++ ytdl.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 from sopel import module, tools
 import os
+import re
 import youtube_dl
 
 
-ydl_opts = {
+ytdl_opts = {
     "format": "bestvideo[height<=?1080]+bestaudio/best",
     "merge_output_format": "mp4",
     "noplaylist": True,
@@ -23,12 +24,34 @@ def ytdl(bot, trigger):
         return
     url = tools.Identifier(url)
 
+    if re.search(r"(youtube\.com|youtub\.be|twitch\.tv)", url):
+        try:
+            with youtube_dl.YoutubeDL(ytdl_opts) as ytdl:
+                meta = ytdl.extract_info(url, download=False)
+                id = meta["id"]
+                ext = meta["ext"]
+                dur = meta["duration"]
+                if not dur:
+                    bot.reply("This video has no duration (livestream?) and cannot be downloaded.")
+                    return
+                if dur > 600:
+                    bot.reply("This video is too long for me to download, sorry!")
+                    return
+                else:
+                    ytdl.download([url])
+                    bot.say("https://actionsack.com/tmp/{}.{}".format(id, ext))
+                    return
+        except youtube_dl.utils.DownloadError:
+            bot.reply("Please submit a valid link.")
+        except KeyError:
+            bot.reply("This video has no duration (livestream?) and cannot be downloaded.")
+
     try:
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            meta = ydl.extract_info(url, download=False)
+        with youtube_dl.YoutubeDL(ytdl_opts) as ytdl:
+            meta = ytdl.extract_info(url, download=False)
             id = meta["id"]
             ext = meta["ext"]
-            ydl.download([url])
+            ytdl.download([url])
             bot.say("https://actionsack.com/tmp/{}.{}".format(id, ext))
     except youtube_dl.utils.DownloadError:
         bot.reply("Please submit a valid link.")
