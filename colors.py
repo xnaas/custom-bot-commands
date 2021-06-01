40827d9533a51aeec43eca10e731cd65f458861e
xnaas <me@xnaas.info>
2021-06-01 14:22:50 -0500

    @module → @plugin, remove pointless returns w/ NOLIMIT

--- colors.py
+++ colors.py
@@ -2,7 +2,7 @@
 Source: https://github.com/xnaas/sopel-color-text
 Forked and heavily modified from: https://github.com/sopel-irc/sopel-rainbow
 """
-from sopel import module, formatting
+from sopel import plugin, formatting
 from sopel.config import types
 import itertools
 import random
@@ -28,7 +28,7 @@ SCHEME_ERRORS = {
 }
 
 
-@module.commands('rainbow', 'usa', 'commie', 'spooky')
+@plugin.commands('rainbow', 'usa', 'commie', 'spooky')
 def rainbow_cmd(bot, trigger):
     """Make text colored. Options are "rainbow", "usa", "commie", and "spooky"."""
     text = clean(trigger.group(2) or '')
@@ -40,7 +40,7 @@ def rainbow_cmd(bot, trigger):
         except KeyError:
             msg = "How did you do that?!"
         bot.reply(msg)
-        return module.NOLIMIT
+        return
 
     try:
         colors = COLOR_SCHEMES[scheme]
@@ -48,7 +48,7 @@ def rainbow_cmd(bot, trigger):
         # not possible to reach this at time of writing, but who knows?
         # mistakes happen when updating stuff that needs to be changed in parallel
         bot.reply("I don't know what color sequence to use for '{}'!".format(scheme))
-        return module.NOLIMIT
+        return
 
     color_cycle = itertools.cycle(colors)
 
